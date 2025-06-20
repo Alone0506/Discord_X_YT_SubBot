508436d82d8746dbc0fb0341459229202bc74b61
Alone0506 <wii03903975@gmail.com>
2025-06-20 21:12:50 +0800

    Fix the issue where X cannot log in after a period of time.

--- api/x_api.py
+++ api/x_api.py
@@ -8,53 +8,28 @@ import sys
 import tweety
 from tweety.types.twDataTypes import Tweet, User
 
-X_USERNAME = os.getenv("X_USERNAME", "")
-X_PASSWORD = os.getenv("X_PASSWORD", "")
-
-# 基礎路徑和會話存儲路徑
-BASE_PATH = Path(__file__).parent.parent
-SESSION_PATH = str(BASE_PATH / 'db' / 'x_session')
 
 logger = logging.getLogger('discord')
 
+
+"""避免get_user_info時出現 OSError: [WinError 6]"""
+logger.info(f"system plarform: {sys.platform}")
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+X_USERNAME = os.getenv("X_USERNAME", "")
+X_PASSWORD = os.getenv("X_PASSWORD", "")
+app = tweety.Twitter(str(Path(__file__).parent.parent / 'db' / 'x_session'))
+initialized = False
+
+
 class XAPI:
-    _instance = None  # 儲存單一實例
-    _initialization_lock = asyncio.Lock()
-    _initialized = False  # 標記是否已初始化
-    
-    def __new__(cls, *args, **kwargs):
-        # 確保只建立一個實例, 避免每次呼叫 XAPI 時都重新 initialze 一次,
-        # 這樣會導致每次都登入一次, 有機會被鎖帳號
-        if cls._instance is None:
-            cls._instance = super(XAPI, cls).__new__(cls)
-        return cls._instance
-    
-    def __init__(self):
-        self.app = tweety.Twitter(SESSION_PATH)
-            
     async def initialize(self):
-        """確保只初始化一次的非同步方法"""
-        if not XAPI._initialized:
-            # 使用鎖確保多個協程同時調用時只執行一次初始化
-            async with XAPI._initialization_lock:
-                # 再次檢查，防止其他協程在等待鎖時已初始化
-                if not XAPI._initialized:
-                    try:
-                        self._setup_event_loop_policy()
-                        
-                        await self.app.start(X_USERNAME, X_PASSWORD)
-                        XAPI._initialized = True
-                        logger.info("XAPI initialized successfully")
-                    except Exception as e:
-                        logger.error(f"Error initializing XAPI: {e}")
-                        
-    def _setup_event_loop_policy(self):
-        """避免get_user_info時出現 OSError: [WinError 6]"""
-        logger.info(f"system plarform: {sys.platform}")
-        if sys.platform == 'win32':
-            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-                        
-        
+        global initialized
+        if not initialized:
+            await app.start(X_USERNAME, X_PASSWORD)
+            initialized = True
+    
     async def get_new_user_info(self, username: str) -> dict[str, str]:
         """
         input:
@@ -68,7 +43,7 @@ class XAPI:
         await self.initialize()
         
         try:
-            user: User = await self.app.get_user_info(username)
+            user: User = await app.get_user_info(username)
             return {
                 "title": user.name,
                 "icon_url": user.profile_image_url_https,
@@ -110,6 +85,8 @@ class XAPI:
                 print(f"發現新推文: {url}")
             ```
         """
+        await self.initialize()
+        
         def valid_tweet(tweet, last_updated: datetime) -> bool:
             if not isinstance(tweet, Tweet):
                 return False
@@ -119,11 +96,9 @@ class XAPI:
                 return False
             return True
         
-        await self.initialize()
-        
         try:
             last_updated: datetime = datetime.fromisoformat(last_updated_str)
-            tweets = await self.app.get_tweets(username)
+            tweets = await app.get_tweets(username)
             valid_tweets = [tweet for tweet in tweets.tweets if valid_tweet(tweet, last_updated)]
             valid_tweets.sort(key=lambda x: x.created_on)
             
@@ -147,4 +122,15 @@ class XAPI:
             
         except Exception as e:
             logger.error(f"Error in x_api.py: get_new_tweets: {e}")
+            logger.error(f"error message: {str(e)}")
+            
+            # 清除舊的 session 檔案
+            if session_path := Path(__file__).parent.parent / 'db' / 'x_session':
+                if session_path.exists():
+                    session_path.unlink()
+                    logger.info(f"Deleted session file: {session_path}")
+            global app
+            app = tweety.Twitter(str(Path(__file__).parent.parent / 'db' / 'x_session'))
+            await app.start(X_USERNAME, X_PASSWORD)
+            
             return [], {}, last_updated_str
